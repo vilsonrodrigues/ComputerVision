dbf4708e39d38a4725a92754c7462628f3b44416
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-05-25 13:10:22 -0300

    Add files via upload

--- ITA_Challenge_2021-TrafficLightDetection/utils.py
+++ ITA_Challenge_2021-TrafficLightDetection/utils.py
@@ -11,9 +11,9 @@ file_dev = open('dev.txt', 'w')
 #Insere no txt
 #O código foi construído para png, caso for aproveitar e seja outro formato, basta alterar
 for path in ['dev','train']:
-	for pathAndFilename in glob.iglob(os.path.join(current_dir+'/'+path, "*.jpg")):
+	for pathAndFilename in glob.iglob(os.path.join(current_dir+'/'+path, "*.png")):
 	    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 	    if path == 'dev':
-	        file_dev.write("data-detection" + "/" + path +'/' + title + '.jpg' + "\n")
+	        file_dev.write("data-detection" + "/" + path +'/' + title + '.png' + "\n")
 	    elif path == 'train':
-	        file_train.write("data-detection" + "/" + path +'/' + title + '.jpg' + "\n")	      
+	        file_train.write("data-detection" + "/" + path +'/' + title + '.png' + "\n")	      

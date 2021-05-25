14753397b43c0ed78eab5c94411f8add43925723
Vilson Rodrigues <vilsonrodrigues07@hotmail.com>
2021-05-25 09:06:14 -0300

    Add files via upload

---
+++ ITA_Challenge_2021-TrafficLightDetection/utils.py
@@ -0,0 +1,19 @@
+import glob, os
+
+#Exibe o diretório atual
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(current_dir)
+
+#Cria os arquivos .txt para armazenar a lista de imagens de treino e dev
+file_train = open('train.txt', 'w')
+file_dev = open('dev.txt', 'w')
+#Insere no txt
+#O código foi construído para png, caso for aproveitar e seja outro formato, basta alterar
+for path in ['dev','train']:
+	for pathAndFilename in glob.iglob(os.path.join(current_dir+'/'+path, "*.jpg")):
+	    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+	    if path == 'dev':
+	        file_dev.write("data-detection" + "/" + path +'/' + title + '.jpg' + "\n")
+	    elif path == 'train':
+	        file_train.write("data-detection" + "/" + path +'/' + title + '.jpg' + "\n")	      
